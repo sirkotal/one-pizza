9e9b14b50bcd6e1559ebf631e20944c375010f8a
sirkotal <lordkotal2002@gmail.com>
2024-03-24 18:24:55 +0000

    Hill Climbing is (kinda) working - missing neighbor generation

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from customer import Customer
+import pizza
 
 def parse_input(input_file):
     clients = []
@@ -19,12 +20,30 @@ def parse_input(input_file):
             disliked = set(line[1:])
             ingredients.update(line[1:])
             clients.append(Customer(liked, disliked))
+    
+    p = pizza.Pizza(clients, ingredients)
 
-    return clients, ingredients
+    return p
 
 if __name__ == '__main__':
-    clients, ingredients = parse_input('../input/a_an_example.in.txt')
+    piz = parse_input('input/a_an_example.in.txt')
     # clients, ingredients = parse_input('../input/c_coarse.in.txt')
-    print(clients)
-    print(ingredients)
-    print(len(ingredients))
+    # print(piz.customers)
+    # print(piz.ingredients)
+    # print(len(piz.ingredients))
+
+    good = set()
+    bad = set()
+
+    for client in piz.customers:
+        good.update(client.likes)
+        bad.update(client.dislikes)
+    
+    good.difference_update(bad)
+
+    # print(good)
+    # print(bad)
+
+    piz.solution = {'peppers', 'cheese', 'mushrooms', 'pineapple', 'tomatoes', 'basil'}
+    print(pizza.hill_climbing(pizza.eval_function, piz).solution)
+    print(pizza.hill_climbing(pizza.eval_function, piz).score)
--- src/pizza.py
+++ src/pizza.py
@@ -1,3 +1,5 @@
+import random
+
 class Pizza:
     def __init__(self, customers, ingredients):
         self.customers = customers
@@ -5,12 +7,34 @@ class Pizza:
         self.solution = set()
         self.score = 0
 
-class TreeNode:
-    def __init__(self, state, parent=None):
-        self.state = state
-        self.parent = parent
-        self.children = []
+def eval_function(pizza): 
+    pizza.score = 0
+    for client in pizza.customers:
+        if ((client.likes & pizza.solution) == client.likes) and ((client.dislikes & pizza.solution) == set()):
+            pizza.score += 1
+    return pizza.score
 
-    def add_child(self, child_node):
-        self.children.append(child_node)
-        child_node.parent = self
+def generate_neighbors(pizza): # steepest ascent, BFS -> TODO
+    neighbors = []
+    
+    return neighbors
+    
+# x0 is the initial solution
+def hill_climbing(f, x0):
+    x = x0
+
+    p1 = Pizza(x0.customers, x0.ingredients)
+    p2 = Pizza(x0.customers, x0.ingredients)
+
+    p1.solution = {'peppers', 'mushrooms', 'pineapple', 'tomatoes', 'basil'}
+    p2.solution = {'peppers', 'cheese', 'mushrooms', 'tomatoes'}
+    while True:
+        # neighbors = generate_neighbors(x)  # generate current solution neighbors
+        neighbors = [p1, p2]
+        best_neighbor = max(neighbors, key=f) # find neighbor with highest fitness value
+        if f(best_neighbor) <= f(x):  # if the best neighbor is not better than the current solution, stop
+            print("P0 Score:", x0.score)
+            print("P1 Score:", p1.score)
+            print("P2 Score:", p2.score)
+            return x
+        x = best_neighbor  # otherwise, continue with the best neighbor
