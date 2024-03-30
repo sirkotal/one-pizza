106a6d1f22133dba0be49f03956643f82b0a947c
pepezunxo <pepestela03@gmail.com>
2024-03-30 18:59:19 +0000

    Added UI.

--- src/main.py
+++ src/main.py
@@ -27,26 +27,70 @@ def parse_input(input_file):
     
     return Pizza(clients, list(ingredients))
 
-if __name__ == '__main__':
+def display_menu():
+    print("------------- OnePizza -------------")
+    print("")
+    print("Welcome to OnePizza!")
+    print("")
+    print("Please choose an optimization algorithm to proceed:")
+    print("")
+    print("1. Hill Climbing")
+    print("2. Simulated Annealing")
+    print("3. Genetic Algorithm")
+    print("4. Tabu Search")
+    print("")
+    print("If you would like to get to know more about OnePizza, press 5.")
+    print("")
+    print("6. Exit")
+    print("")
+    choice = input("Enter your choice (1-6): ")
+    return choice
+
+def main():
     piz = parse_input(os.path.join(os.path.dirname(__file__), '../input/a_an_example.in.txt'))
 
-    # Hill Climbing Testing
-    # result = hill_climbing(eval_function, piz)
-    # print(result.get_solution())
-    # print(result.score)
-    
-    # Simulated Annealing Testing
-    # annealing = simulated_annealing(eval_function, piz)
-    # print(annealing.get_solution())
-    # print(annealing.score)
+    while True:
+        user_choice = display_menu()
+        print("")
+        if user_choice == '1':
+            # Hill Climbing
+            result = hill_climbing(eval_function, piz)
+            print(f"Ingredients: {result.get_solution()}\nScore: {result.score}")
+        elif user_choice == '2':
+            # Simulated Annealing
+            annealing = simulated_annealing(eval_function, piz)
+            print(f"Ingredients: {annealing.get_solution()}\nScore: {annealing.score}")
+        elif user_choice == '3':
+            # Genetic Algorithm
+            neighbors = get_neightbors(piz)
+            best_solution = genetic_algorithm(neighbors, 100, 0.01, "roulette", 4)
+            print(f"Best solution found: {best_solution.get_solution()} ({best_solution.score})")
+        elif user_choice == '4':
+            # Tabu Search
+            tabu_result = tabu_search(piz, eval_function, 1000, 10)
+            print(f"Ingredients: {tabu_result.get_solution()}\nScore: {tabu_result.score}")
+        elif user_choice == '5':
+            # OnePizza explanation
+            print("")
+            print("------------------------------------------------------------------------------------------------------------")
+            print("")
+            print("We are opening a small pizzeria that sells only one pizza option to their customers.")
+            print("So, in order to maximize customers, we need to decide which ingredients our pizza contains.")
+            print("Every customer has liked and disliked ingredients.")
+            print("It is our job to maximize sales by including the ingredients customers like and leave out the disliked ones.")
+            print("Our pizza scores differently based on how many clients come to our shop!")
+            print("Come see which pizza maximizes the customers we get!")
+            print("")
+            print("------------------------------------------------------------------------------------------------------------")
+        elif user_choice == '6':
+            print("Exiting program.")
+            break
+        else:
+            print("")
+            print("Invalid choice, please try again.")
 
-    # Genetic Algorithm Testing
-    # something = get_neightbors(piz)
-    # best_solution = genetic_algorithm(something, 100, 0.01, "roulette", 4)  
-    # print(f"Best solution found: {best_solution.get_solution()} ({best_solution.score})")
+        print("")
+        input("Press any key to return...")
 
-    # Tabu Search Testing
-    # tabu_result = tabu_search(piz, eval_function, 1000, 10)
-    # print("Tabu Search Result:")
-    # print(f"Ingredients: {tabu_result.get_solution()}")
-    # print(f"Score: {tabu_result.score}")
+if __name__ == '__main__':
+    main()
